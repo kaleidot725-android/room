bb1659ce92f3ff1b5f9b61727f368f1c815657cf
Yusuke Katsuragawa <y_katsuragawa@yumemi.co.jp>
2020-05-13 16:44:07 +0900

    Add Simple CRUD

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -2,11 +2,37 @@ package jp.kaleidot725.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            test()
+        }
+    }
+
+    private fun test() {
+        val database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "database-name"
+        ).fallbackToDestructiveMigration().build()
+
+        val userDao = database.userDao()
+        val newUser = User(0, Date().time.toString(), Date().time.toString())
+        userDao.insert(newUser)
+        Log.v("TAG", "after insert ${userDao.getAll()}")
+
+        userDao.deleteAll()
+        Log.v("TAG", "after deleteAll ${userDao.getAll()}")
     }
 }
